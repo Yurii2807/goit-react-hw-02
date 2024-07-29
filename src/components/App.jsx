3f2a936cec2cdc38f7a59de6acbea38e8a611784
Yurii2807 <156546118+Yurii2807@users.noreply.github.com>
2024-07-29 22:16:51 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,8 +39,6 @@ const App = () => {
 
   return (
     <div>
-      <h1>Sip Happens Café</h1>
-      <p>Please leave your feedback about our service by selecting one of the options below.</p>
       <Options
         options={['good', 'neutral', 'bad']}
         onLeaveFeedback={updateFeedback}
