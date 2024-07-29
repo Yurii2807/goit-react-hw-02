0ea85e9565c4e5fb154c6a02558eb76ba1c194de
Yurii2807 <156546118+Yurii2807@users.noreply.github.com>
2024-07-29 20:21:41 +0200

    add Description.file + update Feedback.fileNotification.file

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import css from "./Feedback.module.css";
 
 const Feedback = ({ feedback, totalFeedback, positiveFeedbackPercentage }) => (
-  <div>
-    <p>Good: {feedback.good}</p>
-    <p>Neutral: {feedback.neutral}</p>
-    <p>Bad: {feedback.bad}</p>
-    <p>Total: {totalFeedback}</p>
-    <p>Positive feedback: {positiveFeedbackPercentage}%</p>
+  <div className={css.list}>
+    <p className={css.item}>Good: {feedback.good}</p>
+    <p className={css.item}>Neutral: {feedback.neutral}</p>
+    <p className={css.item}>Bad: {feedback.bad}</p>
+    <p className={css.item}>Total: {totalFeedback}</p>
+    <p className={css.item}>Positive feedback: {positiveFeedbackPercentage}%</p>
   </div>
 );
 
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import css from "./Notification.module.css";
 
-const Notification = ({ message }) => <p>{message}</p>;
+const Notification = ({ message }) => 
+<p className={css.text}>{message}</p>;
 
 Notification.propTypes = {
   message: PropTypes.string.isRequired,
